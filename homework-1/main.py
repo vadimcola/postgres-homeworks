1abe432a9b69ddb5397677dece246a1f4bd80316
Vadim Andreev <vadimcola@mail.ru>
2023-04-21 21:35:17 +0700

    Написан скрипт в `main.py`, который заполняет созданные таблицы данными из `north_data`

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,4 +1,3 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
 
 import csv
@@ -11,7 +10,7 @@ with open('north_data/customers_data.csv', 'r', encoding='utf-8') as file:
 
 with psycopg2.connect(host="localhost", database="north", user="postgres", password="vadim$1983") as conn:
     with conn.cursor() as cur:
-        cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', customer)
+        cur.executemany('INSERT INTO customers VALUES (%s, %s, %s)', customer[1:])
         cur.execute('SELECT * FROM customers')
         rows = cur.fetchall()
         for row in rows:
@@ -20,15 +19,34 @@ with psycopg2.connect(host="localhost", database="north", user="postgres", passw
 conn.close()
 
 
+order = []
+with open('north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=",")
+    for i in file_reader:
+        order.append(tuple(i))
 
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="vadim$1983") as conn:
+    with conn.cursor() as cur:
+        cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', order[1:])
+        cur.execute('SELECT * FROM orders')
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
+conn.close()
 
+employee = []
+with open('north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=",")
+    for i in file_reader:
+        employee.append(tuple(i))
 
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="vadim$1983") as conn:
+    with conn.cursor() as cur:
+        cur.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', employee[1:])
+        cur.execute('SELECT * FROM employees')
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-# orders = []
-# with open('north_data/orders_data.csv', 'r',encoding='utf-8') as file:
-#     file_reader = csv.reader(file, delimiter=",")
-#     for i in file_reader:
-#         customers.append(tuple(i))
-#
-# print(customers[1:])
+conn.close()
